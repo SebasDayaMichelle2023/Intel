39b1e6c1e3a2b54fc0afc16a44cb4c3a0f1726f1
SebasDayaMichelle2023 <140672046+SebasDayaMichelle2023@users.noreply.github.com>
2023-07-28 22:39:55 -0600

    Add files via upload

---
+++ Evidencia.zip/src/conjetura par primer programa.cpp
@@ -0,0 +1,23 @@
+/* conjetura fuerte de numeros primos, toda suma de 2 numeros primos es par*/ 
+
+#include <iostream>
+
+
+using namespace std;
+ 
+ int main(){
+ 	int numero;
+ 	cout<<"digite un numero deseado: " ;
+ 	cin>>numero; 
+ 	
+ 	if (numero%2==0){
+ 		cout<<"EL numero PERTENECE A LA CONJETURA FUERTE";
+ 		}
+ 		else{
+ 			cout<<"Este numero no pertenece a la conjetura fuerte";
+ 			}
+ 			
+ 			
+ 			return 0;
+ 	}
+ 	s
