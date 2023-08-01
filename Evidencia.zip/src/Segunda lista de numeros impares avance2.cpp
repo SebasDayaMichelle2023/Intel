a091b92d980b4202e40069e865c7449c02571b81
SebasDayaMichelle2023 <140672046+SebasDayaMichelle2023@users.noreply.github.com>
2023-08-01 14:13:13 -0600

    Add files via upload

---
+++ Evidencia.zip/src/Segunda lista de numeros impares avance2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	for(i=5; i <=111111; i++){
+		if (i%2==1){
+			cout<< i <<endl;
+		}
+	}
+	 cout << "Presione ENTER para salir..." << endl;
+    cin.get();
+	return 0;
+	
+}
