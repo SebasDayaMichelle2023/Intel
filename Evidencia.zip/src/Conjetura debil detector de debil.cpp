e1a93d6d3ffe033d00b9597db3a224586f69227b
SebasDayaMichelle2023 <140672046+SebasDayaMichelle2023@users.noreply.github.com>
2023-08-01 10:23:17 -0600

    Add files via upload

---
+++ Evidencia.zip/src/Conjetura debil detector de debil.cpp
@@ -0,0 +1,22 @@
+/* conjetura debil de numeros primos, toda suma de 3 numeros primos es impar*/ 
+
+#include <iostream>
+
+
+using namespace std;
+
+ int main(){
+ 	int numero;
+ 	cout<<"digite un numero deseado: " ;
+ 	cin>>numero; 
+
+ 	if (numero%2==0){
+ 		cout<<"EL numero no PERTENECE A LA CONJETURA DEBIL";
+ 		}
+ 		else{
+ 			cout<<"Este numero pertenece a la conjetura debil";
+ 			}
+
+
+ 			return 0;
+ 	}
